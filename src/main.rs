200ba8230953a758866c13d53e7bdf4bd9bf3033
vfilatov <v.filatov@andersenlab.com>
2023-06-17 00:28:43 +0300

    Implement CLI

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,9 @@ fn main() {
     let start = Instant::now();
     let matches = command_line_interface::register_args();
     // Matches the commands and performs actions
-    match matches.subcommand() {
-        Some(("run", sub_m)) => {
-            let (command_type, string) = get_command_type_and_string_args(sub_m);
-            println!("{command_type}, {string}");
-        }
-        _ => (),
+    if let Some(("run", sub_m)) = matches.subcommand() {
+        let (command_type, string) = get_command_type_and_string_args(sub_m);
+        println!("{command_type}, {string}");
     }
     let end = start.elapsed();
     println!("Time spent = {:.2?}", end);
