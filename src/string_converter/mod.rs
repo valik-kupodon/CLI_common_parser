402778bb00aff161c8f37aebde3a818029aa7a0b
vfilatov <v.filatov@andersenlab.com>
2023-06-19 11:26:08 +0300

    Add Unit tests

--- src/string_converter/mod.rs
+++ src/string_converter/mod.rs
@@ -14,3 +14,51 @@ impl QuoteString{
             .collect()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_quoting_string() {
+        let quote_string = QuoteString {};
+        let string = "Hello, World! This is a test string.";
+        let result = quote_string.quoting_string(string);
+        let expected = vec![
+            "Hello".to_string(),
+            "World".to_string(),
+            "This".to_string(),
+            "is".to_string(),
+            "a".to_string(),
+            "test".to_string(),
+            "string".to_string(),
+        ];
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_quoting_string_empty() {
+        let quote_string = QuoteString {};
+        let string = "";
+        let result = quote_string.quoting_string(string);
+        let expected: Vec<String> = vec!["".to_owned()];
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_quoting_string_with_punctuation() {
+        let quote_string = QuoteString {};
+        let string = "Hello, World! This is a test string.";
+        let result = quote_string.quoting_string(string);
+        let expected = vec![
+            "Hello".to_string(),
+            "World".to_string(),
+            "This".to_string(),
+            "is".to_string(),
+            "a".to_string(),
+            "test".to_string(),
+            "string".to_string(),
+        ];
+        assert_eq!(result, expected);
+    }
+}
