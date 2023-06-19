7eb65a501a11c00f4ea046018aa27e78dea62d34
vfilatov <v.filatov@andersenlab.com>
2023-06-19 11:21:53 +0300

    Add quoting of strings

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use crate::command_line_interface::get_command_type_and_string_args;
 use std::time::Instant;
+use crate::string_converter::QuoteString;
 
 mod command_line_interface;
 mod string_converter;
@@ -9,8 +10,9 @@ fn main() {
     let matches = command_line_interface::register_args();
     // Matches the commands and performs actions
     if let Some(("run", sub_m)) = matches.subcommand() {
-        let (command_type, string) = get_command_type_and_string_args(sub_m);
-        println!("{command_type}, {string}");
+        let (_, string) = get_command_type_and_string_args(sub_m);
+        let result = QuoteString::quoting_string(QuoteString {}, string.as_str());
+        println!("{:?}", result);
     }
     let end = start.elapsed();
     println!("Time spent = {:.2?}", end);
--- src/string_converter/mod.rs
+++ src/string_converter/mod.rs
@@ -1,1 +1,16 @@
+pub struct QuoteString{}
 
+impl QuoteString{
+    pub fn quoting_string(self, string: &str) -> Vec<String>{
+        let punctuation_free_string = self.remove_punctuation(string);
+        punctuation_free_string.split(' ')
+            .map(|word| word.to_owned())
+            .collect()
+    }
+
+    fn remove_punctuation(self, string: &str) -> String{
+        string.chars()
+            .filter(|char| !char.is_ascii_punctuation())
+            .collect()
+    }
+}
